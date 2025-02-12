61269b70f3e059dab1dab4dcfe0bf36ec2f18291
RalphMigs <coronelralphmiguel2021@gmail.com>
2025-02-12 15:14:50 +0800

    number_guess.sh

--- Build a Number Guessing Game/number_guess.sh
+++ Build a Number Guessing Game/number_guess.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-#!/bin/bash
 PSQL="psql -X --username=freecodecamp --dbname=number_guess --tuples-only -c"
-echo "Enter your username:"
-read USERNAME
+
 USERNAME_AVAIL=$($PSQL "SELECT username FROM users WHERE username='$USERNAME'") 
 GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM users INNER JOIN games USING(user_id) WHERE username = '$USERNAME'")
 BEST_GAME=$($PSQL "SELECT MIN(number_guesses) FROM users INNER JOIN games USING(user_id) WHERE username = '$USERNAME'")
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE username = '$USERNAME'")
+INSERT_GAME=$($PSQL "INSERT INTO games(number_guesses, user_id) VALUES($GUESS, $USER_ID)")
+
+echo "Enter your username:"
+read USERNAME
+
 if [[ -z $USERNAME_AVAIL ]]
   then
     INSERT_USER=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
@@ -16,7 +20,14 @@ fi
 RANDOM_NUM=$((1 + $RANDOM % 1000))
 GUESS=1
 echo "Guess the secret number between 1 and 1000:"
-  
+
+
+if [[ $GUESS == 1 ]]
+  then
+    echo "You guessed it in $GUESS tries. The secret number was $RANDOM_NUM. Nice job!"
+  else
+    echo "You guessed it in $GUESS tries. The secret number was $RANDOM_NUM. Nice job!"
+fi  
 while read NUM
   do
     if [[ ! $NUM =~ ^[0-9]+$ ]]
@@ -38,12 +49,6 @@ while read NUM
     fi
     GUESS=$(( $GUESS + 1 ))
 done
-if [[ $GUESS == 1 ]]
-  then
-    echo "You guessed it in $GUESS tries. The secret number was $RANDOM_NUM. Nice job!"
-  else
-    echo "You guessed it in $GUESS tries. The secret number was $RANDOM_NUM. Nice job!"
-fi
-USER_ID=$($PSQL "SELECT user_id FROM users WHERE username = '$USERNAME'")
-INSERT_GAME=$($PSQL "INSERT INTO games(number_guesses, user_id) VALUES($GUESS, $USER_ID)")
+
+
 
